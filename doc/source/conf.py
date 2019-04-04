e43594e185ca28a5e2ccd8d14132252aa66bb2f7
chenke <chen.ke14@zte.com.cn>
2019-04-04 15:39:40 +0800

    Solve the error: 'Sphinx' object has no attribute 'info'
    This problem has been discussed in this web:
    http://lists.openstack.org/pipermail/openstack-discuss/2019-April/004462.html
    
    This solution like nova:
    https://review.openstack.org/#/c/648670/
    
    Change-Id: I15e8d731417c041499338ada29c1b7fe5f0715f4

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -33,7 +33,7 @@ extensions = ['redirect',
               'sphinx.ext.todo',
               'sphinx.ext.viewcode',
               'sphinx.ext.mathjax',
-              'oslosphinx',
+              'openstackdocstheme',
               'yasfb',
              ]
 
@@ -103,7 +103,7 @@ man_pages = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
